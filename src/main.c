5ccc2806c6bf87871c51278245e662b047b4e62e
Tymon Knibbe <tknibbe@f0r1s11.codam.nl>
2024-01-17 16:48:13 +0100

    <3 u jan

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: tknibbe <tknibbe@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/01 15:56:28 by tknibbe           #+#    #+#             */
-/*   Updated: 2023/12/13 17:03:29 by tknibbe          ###   ########.fr       */
+/*   Updated: 2024/01/17 16:47:23 by tknibbe          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,6 +36,16 @@ void	draw_floor_and_ceiling(t_textures text, mlx_image_t *bg) //move to
 	}
 }
 
+void	set_icon(mlx_t *mlx) // zet jan als icon
+{
+	mlx_texture_t *pic;
+
+	pic = mlx_load_png("./textures/jan.png");
+	if (!pic)
+		ft_error_and_exit("Icon location not found\n");
+	mlx_set_icon(mlx, pic);
+}
+
 int	main(int argc, char **argv)
 {
 	t_game		game;
@@ -46,6 +56,7 @@ int	main(int argc, char **argv)
 	if (get_input(&game, argv[1]))
 		return (EXIT_FAILURE);
 	initialise_game(&game, argv[1]);
+	set_icon(game.mlx);
 	mlx_loop_hook(game.mlx, key_hook, &game);
 	mlx_loop_hook(game.mlx, fps_counter, &game);
 	mlx_loop(game.mlx);
