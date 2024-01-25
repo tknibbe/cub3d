040144c5699c19bfdd87a8508aa4b095ca343563
Tymon Knibbe <tknibbe@f0r1s11.codam.nl>
2024-01-25 19:49:39 +0100

    hey

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: tknibbe <tknibbe@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/01 15:23:25 by tknibbe           #+#    #+#             */
-/*   Updated: 2024/01/25 19:43:11 by tknibbe          ###   ########.fr       */
+/*   Updated: 2024/01/25 19:48:47 by tknibbe          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@
 # define PI 3.1415926
 
 # include "MLX42.h"
+# include "minimap.h"
 # include <stdio.h>
 # include <stdlib.h>
 
@@ -33,6 +34,7 @@ typedef struct s_textures
 	mlx_texture_t	*east;
 	mlx_texture_t	*south;
 	mlx_texture_t	*west;
+	mlx_texture_t	*icon;
 	int	floor;
 	int	ceiling;
 }	t_textures;
@@ -86,19 +88,17 @@ typedef struct	s_game
 	t_textures	textures;
 	t_player	player;
 	t_images	images;
+	t_minimap	minimap;
 	mlx_t		*mlx;
 }	t_game;
 
 void	key_hook(void *param);
 void	fps_counter(void *mlxshit);
-//void	draw_player(t_game *game, mlx_image_t *img);
-void	draw_player(t_game *game, mlx_image_t *img, int colour);
 void	initialise_game(t_game *game, char *title);
 void	ft_mlx_error_and_exit(t_game *game);
 void	ft_error_and_exit(char *error_str);
 void	free_game_struct(t_game *game);
-void	draw_minimap(t_game *game);
-void	initialize_player(t_game *game);
+void	initialise_player(t_game *game);
 void	put_player_to_window(t_game *game);
 void	draw_rays(t_game *game);
 void	ray_caster(t_game *game);
