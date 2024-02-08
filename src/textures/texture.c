9b8aa58ae747026f06615e702c3bd05552ca6048
Tymon Knibbe <tknibbe@f0r1s5.codam.nl>
2024-02-08 15:12:47 +0100

    fixed norm for texture.c

--- src/textures/texture.c
+++ src/textures/texture.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   texture.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tymonknibbe <tymonknibbe@student.42.fr>    +#+  +:+       +#+        */
+/*   By: tknibbe <tknibbe@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/22 16:10:57 by tknibbe           #+#    #+#             */
-/*   Updated: 2024/02/05 13:07:38 by tymonknibbe      ###   ########.fr       */
+/*   Updated: 2024/02/08 15:11:18 by tknibbe          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,92 +37,55 @@ void	buffer_to_img(int *buffer, mlx_image_t *img)
 	}
 }
 
-static void	draw_wall(t_ray ray, t_game *game, int *y, int *buffer, int x) //fix input to struct
+static void	draw_wall(t_ray ray, t_game *game, int *buffer, int x)
 {
-	double Wallx;
-
 	if (ray.side == 0)
-		Wallx = game->player.pos.y + ray.dist_to_wall * ray.ray.y;
+		ray.wallx = game->player.pos.y + ray.dist_to_wall * ray.ray.y;
 	else
-		Wallx = game->player.pos.x + ray.dist_to_wall * ray.ray.x;
-	Wallx -= floor(Wallx);
-	// printf("final wallx = %f\n", Wallx);
-
-	//adjust code here to pick correct texture
-	int texx = (int) (Wallx * (double) ray.tex_width);
+		ray.wallx = game->player.pos.x + ray.dist_to_wall * ray.ray.x;
+	ray.wallx -= floor(ray.wallx);
+	ray.texx = (int)(ray.wallx * (double)ray.tex_width);
 	if (ray.side == 0 && ray.ray.x > 0)
-		texx = ray.tex_width - texx -1;
+		ray.texx = ray.tex_width - ray.texx -1;
 	if (ray.side == 1 && ray.ray.y < 0)
-		texx = ray.tex_width - texx - 1;
-	// printf ("wall is hit at %f\nx in texture is %d (out of %d)\n", Wallx, texx, game->textures.north->width);
-	
-	
-	double step = (double) ray.tex_height / ray.wall_height;
-
-	// printf("%f vs %d\n", 1.0 * ray.tex_height / ray.wall_height, ray.tex_height / ray.wall_height);
-	double texPos = (ray.wall_start - HEIGHT / 2 + ray.wall_height / 2) * step;
-
-	
-	uint32_t colour; 
-	while (*y < ray.wall_end)
+		ray.texx = ray.tex_width - ray.texx - 1;
+	ray.step = (double) ray.tex_height / ray.wall_height;
+	ray.texpos = (ray.wall_start - HEIGHT / 2 + ray.wall_height / 2) * ray.step;
+	while (ray.y < ray.wall_end)
 	{
-		colour = 0;
-		int texy = (int)texPos;
-		texPos += step;
-		colour = ((int *)ray.texture->pixels)[(ray.tex_height * texy + texx)];
-
-//		colour |= ray.texture->pixels[(ray.tex_height * texy + texx) * 4] << 24;
-//		colour |= ray.texture->pixels[(ray.tex_height * texy + texx) * 4 + 1] << 16;
-//		colour |= ray.texture->pixels[(ray.tex_height * texy + texx) * 4 + 2] << 8;
-//		colour |= ray.texture->pixels[(ray.tex_height * texy + texx) * 4 + 3];
-		// if (ray.side == 1)
-		// 	colour = (colour >> 1) & 8355711;
-		buffer[x * HEIGHT + *y] = colour;
-		*y += 1;
+		ray.texy = (int)ray.texpos;
+		ray.texpos += ray.step;
+		ray.colour = ((int *)ray.texture->pixels) \
+					[(ray.tex_height * ray.texy + ray.texx)];
+		buffer[x * HEIGHT + ray.y] = ray.colour;
+		ray.y++;
 	}
+}
+
+static void	set_texture(t_ray *ray, mlx_texture_t *text)
+{
+	ray->texture = text;
+	ray->tex_height = text->height;
+	ray->tex_width = text->width;
 }
 
 static void	set_texture_direction(t_game *game, t_ray *ray)
 {
 	if (ray->door)
-	{
-		ray->texture = game->textures.door;
-		ray->tex_height = game->textures.door->height;
-		ray->tex_width = game->textures.door->width;
-		return ;
-	}
+		set_texture(ray, game->textures.door);
 	if (ray->side == 0 && ray->ray.x < 0)
-	{
-		ray->texture = game->textures.west;
-		ray->tex_height = game->textures.west->height;
-		ray->tex_width = game->textures.west->width;
-	}
+		set_texture(ray, game->textures.west);
 	else if (ray->side == 0 && ray->ray.x >= 0)
-	{
-		ray->texture = game->textures.east;
-		ray->tex_height = game->textures.east->height;
-		ray->tex_width = game->textures.east->width;
-	}
+		set_texture(ray, game->textures.east);
 	else if (ray->side == 1 && ray->ray.y < 0)
-	{
-		ray->texture = game->textures.north;
-		ray->tex_height = game->textures.north->height;
-		ray->tex_width = game->textures.north->width;
-	}
+		set_texture(ray, game->textures.north);
 	else
-	{
-		ray->texture = game->textures.south;
-		ray->tex_height = game->textures.south->height;
-		ray->tex_width = game->textures.south->width;
-	}
+		set_texture(ray, game->textures.south);
 }
 
 void	line_to_buffer(int *buffer, t_game *game, t_ray ray_vars, int x)
 {
-	int	y;
-
-	y = 0;
-
+	ray_vars.y = 0;
 	ray_vars.wall_height = (int)(HEIGHT / ray_vars.dist_to_wall);
 	ray_vars.wall_start = -ray_vars.wall_height / 2 + HEIGHT / 2;
 	if (ray_vars.wall_start < 0)
@@ -130,17 +93,17 @@ void	line_to_buffer(int *buffer, t_game *game, t_ray ray_vars, int x)
 	ray_vars.wall_end = ray_vars.wall_height / 2 + HEIGHT / 2;
 	if (ray_vars.wall_end >= HEIGHT)
 		ray_vars.wall_end = HEIGHT - 1;
-	
 	set_texture_direction(game, &ray_vars);
-	while (y < ray_vars.wall_start)
+	while (ray_vars.y < ray_vars.wall_start)
 	{
-		buffer[x * HEIGHT + y] = game->textures.ceiling;
-		y++;
+		buffer[x * HEIGHT + ray_vars.y] = game->textures.ceiling;
+		ray_vars.y++;
 	}
-	draw_wall(ray_vars, game, &y, buffer, x);
-	while (y < HEIGHT)
+	draw_wall(ray_vars, game, buffer, x);
+	ray_vars.y = ray_vars.wall_end;
+	while (ray_vars.y < HEIGHT)
 	{
-		buffer[x * HEIGHT + y] = game->textures.floor;
-		y++;
+		buffer[x * HEIGHT + ray_vars.y] = game->textures.floor;
+		ray_vars.y++;
 	}
 }
