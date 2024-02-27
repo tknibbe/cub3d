e9dd6380e363499f776e2d1165f13eedb49772bb
Tymon Knibbe <tknibbe@f0r4s8.codam.nl>
2024-02-27 18:20:27 +0100

    fixed empty lines inside of the map.

--- src/input_handling/get_map/get_map.c
+++ src/input_handling/get_map/get_map.c
@@ -6,7 +6,7 @@
 /*   By: tknibbe <tknibbe@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/06 14:53:33 by tknibbe           #+#    #+#             */
-/*   Updated: 2024/02/15 17:01:35 by tknibbe          ###   ########.fr       */
+/*   Updated: 2024/02/27 18:18:51 by tknibbe          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,21 @@ static void	check_invalid_chars(char *line)
 	}
 }
 
+static char	*get_first_valid_line(int fd)
+{
+	char	*line;
+
+	while (1)
+	{
+		line = get_next_line(fd);
+		if (!line)
+			ft_error_and_exit("Error in GNL()\n");
+		if (!ft_isspace(line[0]))
+			return (line);
+		free(line);
+	}
+}
+
 t_map	*put_in_linked_list(int fd)
 {
 	char	*line;
@@ -37,20 +52,18 @@ t_map	*put_in_linked_list(int fd)
 	t_map	*map;
 
 	map = NULL;
+	line = get_first_valid_line(fd);
 	while (1)
 	{
+		line = ft_strdel(line, "\n");
+		if (!line[0])
+			ft_error_and_exit("Error, invalid input in map found\n");
+		check_invalid_chars(line);
+		temp = ft_lstnew_map(line);
+		ft_mapadd_back(&map, temp);
 		line = get_next_line(fd);
 		if (!line)
 			break ;
-		line = ft_strdel(line, "\n");
-		if (line[0])
-		{
-			check_invalid_chars(line);
-			temp = ft_lstnew_map(line);
-			ft_mapadd_back(&map, temp);
-		}
-		else
-			free(line);
 	}
 	if (errno != 0)
 		ft_error_and_exit("Malloc failure\n");
